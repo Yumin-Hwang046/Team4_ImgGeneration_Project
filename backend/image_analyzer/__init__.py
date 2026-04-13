682f8b78670d4857c9906c0f3cdacb6ddddd3644
Yumin Hwang <149315511+Yumin-Hwang046@users.noreply.github.com>
2026-04-13 09:18:11 +0900

    Revert "이미지 생성 모델 실험"

--- backend/image_analyzer/__init__.py
+++
@@ -1,8 +0,0 @@
-from .analyzer import analyze_and_generate_prompts, AnalysisResult, MOOD_CONFIGS, resolve_mood
-
-__all__ = [
-    "analyze_and_generate_prompts",
-    "AnalysisResult",
-    "MOOD_CONFIGS",
-    "resolve_mood",
-]
--- backend/text_generator/__init__.py
+++
@@ -1,8 +0,0 @@
-from .generator import generate_copy_from_image, generate_copy_from_text, CopyResult, CopyVariant
-
-__all__ = [
-    "generate_copy_from_image",
-    "generate_copy_from_text",
-    "CopyResult",
-    "CopyVariant",
-]
